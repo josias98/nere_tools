0da527fdffdf84b42325cabdee01e0eb1e2f43bd
josias98 <diamitanijosiasmansour@gmail.com>
2026-07-18 11:40:59 +0000

    feat: set platform favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? config('app.name', 'Nere Tools') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('brand/favicon.png') }}">
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
